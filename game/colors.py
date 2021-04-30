c1ac91d960ba7735e6d0c7f4eca7d569d89afba6
horus-4ever <th.pichard54@gmail.com>
2021-04-30 11:53:29 +0200

    added new color for piece visualisation

--- game/colors.py
+++ game/colors.py
@@ -8,8 +8,8 @@ class Colors(IntEnum):
     Cette énumération contient les différentes couleurs utilisées dans le jeu.
 
     La méthode 'random_color' retourne une couleur aléatoire parmi les membres
-    de l'énumération, à l'exception de quatre couleurs réservées (DARK_GREY,
-    BLACK, WHITE et LIGHT_GREY).
+    de l'énumération, à l'exception de cinq couleurs réservées (DARK_GREY,
+    BLACK, WHITE, LIGHT_GREY et VERY_DARK_GREY).
     """
     ORANGE = Color(0xFF5500)
     YELLOW = Color(0xFFFF00)
@@ -19,9 +19,10 @@ class Colors(IntEnum):
     RED = Color(0xFF0000)
     PURPLE = Color(0xEE00EE)
     DARK_GREY = Color(0x555555)
+    VERY_DARK_GREY = Color(0x333333)
     LIGHT_GREY = Color(0xAAAAAA)
     BLACK = Color(0x000000)
 
     @classmethod
     def random_color(cls):
-        return choice(list(set(cls.__members__.values()) - {cls.DARK_GREY, cls.BLACK, cls.WHITE, cls.LIGHT_GREY}))
+        return choice(list(set(cls.__members__.values()) - {cls.DARK_GREY, cls.BLACK, cls.WHITE, cls.LIGHT_GREY, cls.VERY_DARK_GREY}))
